6e89a4dbbe91b4ba8b67dfe884f13930fd407334
Lucas Félix <83433449+LucasF4@users.noreply.github.com>
2022-12-21 16:58:29 -0300

    Update connection.js

--- Database/connection.js
+++ Database/connection.js
@@ -1,9 +1,9 @@
 const knex = require('knex')({
-  client: 'oracledb',
+  client: '',
   connection: {
-    user: 'carvalho',
-    password: 'c5prod2013gc',
-    connectString: '(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 172.30.1.15)(PORT = 1521))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = CON5PROD_iad18f.zndb.vcndb.oraclevcn.com)))'
+    user: '',
+    password: '',
+    connectString: ''
   },
   fetchAsString: ["number", "clob"]
 })
